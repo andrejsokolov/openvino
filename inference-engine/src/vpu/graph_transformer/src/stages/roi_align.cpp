9703b7bf5b285c4b69fdf612aeba99c0bded0dc4
Sokolov, Andrey <andrey.sokolov@intel.com>
2020-08-20 18:52:27 +0300

    ROIAlign : use CHWc version

--- inference-engine/src/vpu/graph_transformer/src/stages/roi_align.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/roi_align.cpp
@@ -22,6 +22,7 @@ static const char s_pooled_w[] = "pooled_w";
 static const char s_pooled_h[] = "pooled_h";
 static const char s_sampling_ratio[] = "sampling_ratio";
 static const char s_spatial_scale[] = "spatial_scale";
+static const char s_use_buffer[] = "use_buffer";
 
 namespace {
 
@@ -62,11 +63,14 @@ private:
         const auto spatial_scale = attrs().get<float>(s_spatial_scale);
         const auto mode = attrs().get<ROIAlignMode>(s_mode);
 
+        const auto use_buffer = attrs().get<bool>(s_use_buffer);
+
         serializer.append(static_cast<uint32_t>(pooled_w));
         serializer.append(static_cast<uint32_t>(pooled_h));
         serializer.append(static_cast<uint32_t>(sampling_ratio));
         serializer.append(static_cast<float>(spatial_scale));
         serializer.append(static_cast<ROIAlignMode>(mode));
+        serializer.append(static_cast<uint32_t>(use_buffer));
     }
 
     void serializeDataImpl(BlobSerializer& serializer) const override {
@@ -74,7 +78,12 @@ private:
             inputEdge(i)->input()->serializeBuffer(serializer);
         }
 
+        const auto use_buffer = attrs().get<bool>(s_use_buffer);
         outputEdge(0)->output()->serializeBuffer(serializer);
+        // const int width = inputEdge(0)->input()->desc().dim(Dim::W);
+        // if (width >= 200)
+        if (use_buffer)
+            tempBuffer(0)->serializeBuffer(serializer);
     }
 };
 
@@ -104,6 +113,19 @@ void FrontEnd::parseROIAlign(const Model& model, const ie::CNNLayerPtr& layer, c
     stage->attrs().set<int>(s_pooled_h, layer->GetParamAsInt("pooled_h"));
     stage->attrs().set<int>(s_sampling_ratio, layer->GetParamAsInt("sampling_ratio"));
     stage->attrs().set<float>(s_spatial_scale, layer->GetParamAsFloat("spatial_scale"));
+
+    int buffer_size = 30 * 1024 * 1024;
+    const int width = inputs[0]->desc().dim(Dim::W);
+
+    // const int height = inputs[0]->desc().dim(Dim::H);
+    // const int channels = inputs[0]->desc().dim(Dim::C);
+    bool use_buffer = width >= 200;
+    // use_buffer = false;
+    // if (width >= 200)
+    if (use_buffer)
+        model->addTempBuffer(stage, DataDesc({buffer_size}));
+
+    stage->attrs().set<bool>(s_use_buffer, use_buffer);
 }
 
 }  // namespace vpu
