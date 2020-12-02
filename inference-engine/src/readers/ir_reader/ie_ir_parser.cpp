060f7392e82fd4bbeff3831f0a26ea51fff724dc
Pavel Esir <pavel.esir@intel.com>
2020-12-02 18:26:15 +0300

    Initial support of GatherElements in MO and nGraph

--- inference-engine/src/readers/ir_reader/ie_ir_parser.cpp
+++ inference-engine/src/readers/ir_reader/ie_ir_parser.cpp
@@ -71,6 +71,7 @@ V10Parser::V10Parser(const std::vector<IExtensionPtr>& exts) : _exts(exts) {
     opsets["opset3"] = ngraph::get_opset3();
     opsets["opset4"] = ngraph::get_opset4();
     opsets["opset5"] = ngraph::get_opset5();
+    opsets["opset6"] = ngraph::get_opset6();
 
     // Load custom opsets
     for (const auto& ext : exts) {
@@ -451,7 +452,7 @@ std::shared_ptr<ngraph::Node> V10Parser::createNode(const std::vector<ngraph::Ou
 
     // Check that operation in default opsets
     auto isDefaultOpSet = [](const std::string& version) -> bool {
-        for (size_t i = 1; i <= 5; i++) {
+        for (size_t i = 1; i <= 6; i++) {
             std::string opset_name = "opset" + std::to_string(i);
             if (version == opset_name)
                 return true;
